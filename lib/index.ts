befb1c7f8dec648758f1cad1b1afe734dfe8532b
Reinhardt <notmarx.tech@gmail.com>
2021-12-26 16:55:23 +0700

    Set `VERSION` as type string

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,4 @@ export * as Giveaways from "./giveaways";
 export * as Util from "./util";
 
 // eslint-disable-next-line
-export const VERSION = require("../package.json").version;
+export const VERSION: string = require("../package.json").version;
