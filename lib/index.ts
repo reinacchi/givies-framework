b20acc1db808ecace74971ab8e5eff41e4bcc081
Reinhardt <notmarx.tech@gmail.com>
2022-03-14 16:54:56 +0700

    Update `eslint-disable` comment

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,5 @@ export * as Giveaways from "./giveaways";
 export * as Images from "./images";
 export * as Utils from "./utils";
 
-// eslint-disable-next-line
+/* eslint-disable-next-line */
 export const VERSION: string = require("../package.json").version;
