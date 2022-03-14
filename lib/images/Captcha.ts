c3a374c87264399875fd60c00a77d4f6089a7158
Reinhardt <notmarx.tech@gmail.com>
2022-03-14 16:55:15 +0700

    Add JSDoc and format code

--- lib/images/Captcha.ts
+++ lib/images/Captcha.ts
@@ -21,23 +21,36 @@ interface CaptchaOptions {
     minLines?: number;
 }
 
+/**
+ * Represents a Captcha class constructor
+ */
 export class Captcha {
-    width: number;
     height: number;
+
     length: number;
+
     opt: CaptchaOptions;
 
+    width: number;
+
+    /**
+     * Represents a Captcha class constructor
+     * @param width The width of the captcha
+     * @param height The height of the captcha
+     * @param length The length character of the captcha
+     * @param options Options to extend the Captcha option
+     */
     constructor(width: number, height: number, length: number, options?: CaptchaOptions) {
         this.length = length;
         this.width = width;
         this.height = height;
 
         const opt: CaptchaOptions = Object.assign({
-            fontFamily: 'comismsh',
+            fontFamily: "comismsh",
             lineWidth: 2,
-            chars: '0123456789abcdefghjknpqrstuvxyzABCDEFGHJKLNPQRSTUVXYZ',
-            colorHex: '23456789',
-            bgColor: '#7289DA',
+            chars: "0123456789abcdefghjknpqrstuvxyzABCDEFGHJKLNPQRSTUVXYZ",
+            colorHex: "23456789",
+            bgColor: "#7289DA",
             maxLines: 4,
             minLines: 2
         }, options);
@@ -49,6 +62,18 @@ export class Captcha {
         this.opt = opt;
     }
 
+    private _randomChars(): string {
+        return _.sampleSize(this.opt.chars, this.length).join("");
+    }
+
+    private _randomColor() {
+        return "#" + _.sampleSize(this.opt.colorHex, 3).join("");
+    }
+
+    /**
+     * Create a Captcha
+     * @returns {AdvancedCaptcha}
+     */
     createCaptcha(): AdvancedCaptcha {
         const { canvas, ctx } = this.initCanvas();
         this.drawLine(ctx);
@@ -57,6 +82,47 @@ export class Captcha {
         return { buffer: buffer, text: text };
     }
 
+    /**
+     * Draw a line for the Captcha
+     * @param ctx The canvas context
+     */
+    drawLine(ctx: any) {
+        ctx.lineWidth = this.opt.lineWidth;
+        ctx.globalAlpha = 0.3;
+
+        for (let i = 0; i < this.length; i++) {
+            ctx.strokeStyle = this._randomColor();
+            ctx.beginPath();
+            ctx.moveTo(0, _.random(0, this.height));
+            ctx.lineTo(this.width, _.random(0, this.height));
+            ctx.stroke();
+        }
+
+    }
+
+    /**
+     * Draw a text for the Captcha
+     * @param ctx The canvas context
+     * @returns
+     */
+    drawText(ctx: any) {
+        const text = this._randomChars();
+        ctx.globalAlpha = 1;
+
+        for (let i = 0; i < this.length; i++) {
+            ctx.font = `${this.opt.fontSize}px ${this.opt.fontFamily}`;
+            ctx.fillStyle = "#23272A";
+            const x = this.opt.borderWidth + (i + Math.random() / 2 - 0.25) * this.opt.wordSpace;
+            const y = this.height * 0.8;
+            ctx.fillText(text[i], x, y);
+        }
+
+        return text;
+    }
+
+    /**
+     * Initialize Canvas
+     */
     initCanvas() {
         const canvas = createCanvas(this.width, this.height);
         const ctx = canvas.getContext("2d");
@@ -66,41 +132,4 @@ export class Captcha {
 
         return { canvas, ctx };
     }
-
-    drawText(ctx: any) {
-        const text = this._randomChars();
-        ctx.globalAlpha = 1;
-
-        for (let i = 0; i < this.length; i++) {
-            ctx.font = `${this.opt.fontSize}px ${this.opt.fontFamily}`;
-            ctx.fillStyle = '#23272A'
-            const x = this.opt.borderWidth + (i + Math.random() / 2 - 0.25) * this.opt.wordSpace;
-            const y = this.height * 0.8;
-            ctx.fillText(text[i], x, y);
-          }
-      
-          return text;
-    }
-
-    drawLine(ctx) {
-        ctx.lineWidth = this.opt.lineWidth;
-        ctx.globalAlpha = 0.3;
-    
-        for (let i = 0; i < this.length; i++) {
-          ctx.strokeStyle = this._randomColor();
-          ctx.beginPath();
-          ctx.moveTo(0, _.random(0, this.height));
-          ctx.lineTo(this.width, _.random(0, this.height));
-          ctx.stroke();
-        }
-
-      }
-
-    private _randomChars(): string {
-        return _.sampleSize(this.opt.chars, this.length).join("");
-    }
-
-    private _randomColor() {
-        return '#' + _.sampleSize(this.opt.colorHex, 3).join('');
-      }
 }
